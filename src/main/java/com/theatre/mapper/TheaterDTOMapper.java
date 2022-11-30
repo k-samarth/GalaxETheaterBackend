e16d796c49bf4b3aaa4728bb455e77d02729a456
samarth.narasimha <samarth.narasimha@skillassure.com>
2022-11-30 09:40:37 +0530

    Nothing to Update

--- src/main/java/com/theatre/mapper/TheaterDTOMapper.java
+++ src/main/java/com/theatre/mapper/TheaterDTOMapper.java
@@ -1,5 +1,22 @@
 package com.theatre.mapper;
 
-public interface TheaterDTOMapper {
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+import com.theatre.dto.TheaterDTO;
+import com.theatre.entity.Theater;
+
+@Mapper
+public interface TheaterDTOMapper {
+	@Mapping(target = "code", source = "theater.code")
+	@Mapping(target = "name", source = "theater.name")
+	@Mapping(target = "imgUrl", source = "theater.imgUrl")
+	@Mapping(target = "address", source = "theater.address")
+	public TheaterDTO convertToDto(Theater theater);
+	
+	@Mapping(target = "code", source = "code")
+	@Mapping(target = "name", source = "name")
+	@Mapping(target = "imgUrl", source = "imgUrl")
+	@Mapping(target = "address", source = "address")
+	public Theater convertToEntity(TheaterDTO theater);
 }
