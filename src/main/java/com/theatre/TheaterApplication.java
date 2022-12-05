528bfa6a8f68575e5b753eeb22f5ed646b086fd3
Vishal_B <vishal.b@pratian.com>
2022-12-05 09:31:26 +0000

    Update TheaterApplication.java

--- src/main/java/com/theatre/TheaterApplication.java
+++ src/main/java/com/theatre/TheaterApplication.java
@@ -3,10 +3,8 @@ package com.theatre;
 // imports
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 // Starting Point for The Backend Application
-@EnableEurekaClient
 @SpringBootApplication
 public class TheaterApplication {
 
